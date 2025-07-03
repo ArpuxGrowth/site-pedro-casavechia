1ed241286f638cde62af5c72894e89eee2c11d2c
Arpux <arpux.growth@gmail.com>
2025-07-03 16:32:18 -0300

    teste vercel.j n2

--- js/prismic-blog.js
+++ js/prismic-blog.js
@@ -19,12 +19,12 @@ blogs.forEach((blog, i) => {
     article.innerHTML = `
         <div class="blog-post">
             <div class="blog-post-image bg-fast-blue">
-                <a href=blog/post.html?uid=${uid}><img src=${image} alt=""></a>
+                <a href=blog/${uid}><img src=${image} alt=""></a>
             </div>
             <div class="post-details bg-white text-center padding-3-rem-all xl-padding-2-rem-all">
-                <a href=blog/post.html?uid=${uid} class="blog-category text-fast-blue margin-15px-bottom text-medium font-weight-500 letter-spacing-1px text-uppercase">Artigo</a>
-                <a href=blog/post.html?uid=${uid} class="alt-font text-extra-dark-gray text-extra-dark-gray-hover text-large line-height-26px d-block margin-20px-bottom">${title}</a>
-                <a href=blog/post.html?uid=${uid} class="alt-font text-uppercase text-extra-small letter-spacing-1px d-block">${date}</a>
+                <a href=blog/${uid} class="blog-category text-fast-blue margin-15px-bottom text-medium font-weight-500 letter-spacing-1px text-uppercase">Artigo</a>
+                <a href=blog/${uid} class="alt-font text-extra-dark-gray text-extra-dark-gray-hover text-large line-height-26px d-block margin-20px-bottom">${title}</a>
+                <a href=blog/${uid} class="alt-font text-uppercase text-extra-small letter-spacing-1px d-block">${date}</a>
             </div>
         </div>
     `
